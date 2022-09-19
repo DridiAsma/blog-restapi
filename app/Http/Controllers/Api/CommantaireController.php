ef26bd7a50ca4109ee6132366dd9707451fb20c0
DridiAsma <asma.dridi@esen.tn>
2022-09-19 09:22:39 +0100

    first commit

--- app/Http/Controllers/Api/CommantaireController.php
+++ app/Http/Controllers/Api/CommantaireController.php
@@ -4,8 +4,110 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Commantaire;
+use App\Models\Post;
 
 class CommantaireController extends Controller
 {
-    //
+    public function index($id){
+        $post = Post::find($id);
+        if(!$post)
+        {
+            return response([
+                'message' => 'post not found'
+            ], 403);
+
+        }
+
+        return response([
+            'comments' => $post->comments()->with('user:id,name,image')->get()
+        ], 200);
+    }
+
+// create
+    public function store(Request $request, $id)
+    {
+        $post = Post::find($id);
+
+        if(!$post){
+            return response([
+                'message' => 'Post not found'
+            ], 403);
+        }
+        //validate fields
+        $valide = $request->validate([
+            'comment' => 'required|string'
+        ]);
+
+        Commantaire::create([
+            'comment' => $valide['comment'],
+            'post_id' => $id,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return response([
+            'message' => 'comment created'
+        ], 200);
+    }
+
+    //update
+    public function update(Request $request, $id)
+    {
+        $comment = Commantaire::find($id);
+
+        if(!$comment)
+        {
+            return response([
+                'message' => 'Comment not found.'
+            ], 403);
+        }
+
+        if($comment->user_id != auth()->user()->id)
+        {
+            return response([
+                'message' => 'Permission denied.'
+            ], 403);
+        }
+
+        //validate fields
+        $attrs = $request->validate([
+            'comment' => 'required|string'
+        ]);
+
+        $comment->update([
+            'comment' => $attrs['comment']
+        ]);
+
+        return response([
+            'message' => 'Comment updated.'
+        ], 200);
+    }
+
+    // delete a comment
+    public function destroy($id)
+    {
+        $comment = Commantaire::find($id);
+
+        if(!$comment)
+        {
+            return response([
+                'message' => 'Comment not found.'
+            ], 403);
+        }
+
+        if($comment->user_id != auth()->user()->id)
+        {
+            return response([
+                'message' => 'Permission denied.'
+            ], 403);
+        }
+
+        $comment->delete();
+
+        return response([
+            'message' => 'Comment deleted.'
+        ], 200);
+    }
+
+
 }
--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,90 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    //get all
+    public function index()
+    {
+        return response([
+            'posts' => Post::orderBy('created_at', 'desc')->
+            with('user:id,name,image')->withCount('comments', 'likes')->get()
+        ], 200);
+    }
+
+ //get single post
+ public function show($id)
+ {
+    return response([
+        'post' => Post::where('id', $id)->
+        withCount('comments', 'likes')->get()
+    ], 200);
+ }
+
+
+ //create posts
+ public function store(Request $request){
+
+    $valide = $request->validate([
+        'body' => 'required|string'
+    ]);
+
+    $post = Post::create([
+        'body' => $valide['body'],
+        'user_id' => auth()->user()->id
+    ]);
+
+    return response(['message' => 'Post created', 'post' => $post]);
+ }
+
+
+
+ public function update(Request $request, $id)
+ {
+    $post = Post::find($id);
+    if(!$post)
+    {
+        return response([
+            'message' => 'Post not found'
+        ], 403);
+    }
+
+    //validate fields
+    $valide = $request->validate([
+        'body' => 'required|string'
+    ]);
+
+    //for now skip for post image
+    return response([
+        'message' => 'post Update',
+        'post' => $post
+    ], 200);
+
+ }
+
+ public function delete($id)
+ {
+    $post = Post::find($id);
+    if(!$post)
+    {
+    return response(['message' => 'post not foind.'], 403);
+    }
+    if($post->user_id != auth()->user()->id)
+    {
+        return response([
+            'message' => 'Permission denied.'
+        ], 403);
+    }
+
+    $post->comments()->delete();
+    $post->likes()->delete();
+    $post->delete();
+
+    return response([
+        'message' => 'post deleted'
+    ], 200);
+ }
 }
